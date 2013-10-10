8cc4fa2ffb4cbfece564c3e4bac3e25e5eed9f92
Monika <mon.kostolna@gmail.com>
2013-10-10 10:51:25 +0200

    school change

--- app.rb
+++ app.rb
@@ -1,2 +1,29 @@
 #!/usr/bin/env ruby
 #encoding:utf-8
+#require 'net/http'
+#require 'json'
+#require 'uri'
+#
+#exit 0 if ARGV.empty?
+#
+#GRAPH_API_URl = 'http://graph.facebook.com/'
+#FB_API_URL = 'http://api.facebook.com'
+#
+#def ok?(response)
+#response.code == '200'
+#
+# unless base_domain
+# p "WRONG"
+# exit 0
+# end
+# p "BASE DOMAIN is '#{base_domain}'"
+#
+# uri_for_pge = "#{GRAPH_API_URI}idnes.cz"
+# p uri_for_page
+# response = Net::HTTP.get_response(URI.parse(uri_for_page))
+# response.code
+# p JSON.parse(response.body)['id']
+#
+#
+#
+#
